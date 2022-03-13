121abb33616e3cc7123b6d152184093fcbd13918
Abdolrahman EL-hagrasy <93329344+Body661@users.noreply.github.com>
2022-03-13 21:54:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ toggleMenu.onclick = function () {
       : (links.style.display = "flex");
   }
 };
+
+let section = document.querySelector(".skills");
+let spans = document.querySelectorAll(".prog span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 200) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
+  }
+};
